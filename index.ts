d277ac44447b1d6e71021079f76986cf1b450a52
James Murdza <james@jamesmurdza.com>
2023-12-22 18:58:00 -0800

    Set sandbox timeout to three minutes.

--- index.ts
+++ index.ts
@@ -4,16 +4,17 @@ import dotenv from "dotenv";
 dotenv.config();
 
 (async () => {
-  // Spawn your custom sandbox
-  const sandbox = await Sandbox.create({ template: "react-evals" });
+  const sandbox = await Sandbox.create({
+    template: "react-evals",
+    cwd: "/evals",
+    timeout: 3 * 60 * 1000, // 3 minutes
+  });
 
   const procWithCustomHandler = await sandbox.process.start({
-    cmd: "cd /evals && npm start",
+    cmd: "npm start",
     onStdout: (data) => console.log("process", data.line),
-    timeout: 3*60*1000, // 3 minutes
   });
   await procWithCustomHandler.wait();
 
-  // Close sandbox once done
   await sandbox.close();
 })();
