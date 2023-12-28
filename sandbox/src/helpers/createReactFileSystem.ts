27428391c23fff858b6d518f1a60749031e25834
James Murdza <james@jamesmurdza.com>
2023-12-27 16:41:03 -0800

    Remove dependency detection from the sandbox.

--- sandbox/src/helpers/createReactFileSystem.ts
+++ sandbox/src/helpers/createReactFileSystem.ts
@@ -1,7 +1,6 @@
 import fs from 'fs';
 import path from 'path';
 import tmp from 'tmp';
-import { packageDotJSONFromAppDotJS } from "./gitwit";
 
 interface TmpDirObject {
   name: string;
@@ -13,7 +12,6 @@ export function createTemporaryFileSystem(): Record<string, any> {
   const reactAppDirObj = tmp.dirSync({ unsafeCleanup: true });
   const reactAppDir: string = reactAppDirObj.name;
   fs.cpSync(path.join(__dirname, '../../app'), reactAppDir, { recursive: true });
-  fs.writeFileSync(path.join(reactAppDir, 'package.json'), packageDotJSONFromAppDotJS(fs.readFileSync(path.join(reactAppDir, 'src', 'App.js'), 'utf8')));
 
   return {
     reactAppDirObj: reactAppDirObj,
